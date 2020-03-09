b3618693f3e201a1bfe1394385b433ceaa3834e1
bitato2 <61390457+bitato2@users.noreply.github.com>
2020-03-09 22:15:16 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,28 +1,74 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-var Channels = ["686244189167550488","686244230712000516","686244262802882575","686244301680017629","686244374153003091","686244409041223938","686253539718332486","686244487940669567","686244516717658147","686244572992634905","686244639623217179","686244674473689110","686244839406174215","686244886621585523"];
-console.log(Channels[0])
 var Token = process.env.BOT_TOKEN;
-var TimeBetween = .002;
-var Total = 0
+var Admins = ["350638288702275586"];
+const prefix = "bba!"
+var AnnouncementsChannel = null
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
+  client.channels.find("name", "admin-chat").send(
+  "**━━━━━[ BIG BRAIN BOT ]━━━━━**\n"+
+  "**-** Prefix: "+prefix+"\n"+
+  "**-** To see a list of commands type: "+prefix+"help");
 })
 client.on('message', message => {
-  var Valid = false;
-  var ArrayLength = Channels.length
-  for (var i = 0; i < ArrayLength; i++) {
-    var Current = Channels[i]
-    if (message.channel.id === Current) {
-      Valid = true;
+  let guild = client.guilds.get('686215263670108219')
+  var BotName = client.user.username
+  var Author = message.author.username
+  var Verified = false
+  for (var i = 0; i < Admins.length; i++){
+    var CurrentItem = Admins[i]
+    if (message.member.user.id==CurrentItem){
+      Verified = true
       break
     }
   }
-  if (Valid) {
-    Total++
-    const Message = 'Adverts: '+(Total-1).toString()
-    console.log(Message)
-    message.guild.channels.find("name", Message).setName("Adverts: "+Total.toString());
+  if (BotName.toString()!=Author.toString()&&Verified){
+    if (message.content==prefix+"help"){
+      message.channel.send(
+        "**━━━━━[ BIG BRAIN BOT ]━━━━━**\n"+
+        prefix+"`help`\n"+
+        "• View a list of commands.\n"+
+        prefix+"`admin`\n"+
+        "• Allow someone to use this bot, second arg user id.\n"+
+        prefix+"`setannounce`\n"+
+        "• Set the announcements channel, second arg #channel.\n"+
+        prefix+"`announce`\n"+
+        "• Announce in the announcements channel, current: "+AnnouncementsChannel+". Second arg announcement."
+      )
+      return
+    }
+    const args = message.content.slice(prefix.length).split(' ');
+    const PlayerMention = message.mentions.users.first();
+    const ChannelTag = message.mentions.channels.first();
+    const command = args.shift().toLowerCase();
+    if (command=="admin"&&PlayerMention){
+      var Found = false;
+      for (var i = 0;i < Admins.length;i++){
+        var CurrentItem = Admins[i]
+        if (CurrentItem==PlayerMention.id){
+          Found = true
+          break
+        }
+      }
+      if (!Found){
+        Admins.push(PlayerMention.id)
+      }
+    }
+    if (command=="setannounce"&&ChannelTag){
+      AnnouncementsChannel = ChannelTag
+      message.channel.send("Epic! Set the announcements channel to: "+AnnouncementsChannel)
+    }
+    if (command=="announce"&&args[0]){
+      if (!AnnouncementsChannel){
+        message.channel.send("Error! No announcements channel set.")
+      }else{
+        message.channel.send("Epic! Sending announcement.\n"+
+        "`"+message.content.slice(prefix.length+9)+"`"
+        )
+        AnnouncementsChannel.send(message.content.slice(prefix.length+8))
+      }
+    }
   }
 });
 client.login(Token);
